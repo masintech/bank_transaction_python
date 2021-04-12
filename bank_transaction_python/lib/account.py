b053c88904c0444880f80f752d049455576e5e14
weijeh <weijeh@avery-design.com.tw>
2021-04-12 19:01:58 +0800

    [lib]: first implementation on bank/account Project: Note   :

--- bank_transaction_python/lib/account.py
+++ bank_transaction_python/lib/account.py
@@ -6,21 +6,24 @@ class Account:
     MIN_BALANCE = 500
     NextAccountNumber = 0
     # private varible will be named with _ as prefix
-    def __init__(self, fname, lname, balance, NextAccountNumber):
-        NextAccountNumber += 1
-        self._accountNumber = NextAccountNumber
+    def __init__(self, fname="", lname="", balance=0):
+        Account.NextAccountNumber += 1
+        self._accountNumber = Account.NextAccountNumber
         self._firstName = fname
         self._lastName = lname
         self._balance = balance
 
+    def getAccNo(self):
+        return self._accountNumber
+
     def getFirstName(self):
-        return _firstName
+        return self._firstName
 
     def getLastName(self):
-        return _lastName
+        return self._lastName
 
     def getBalance(self):
-        return _balance
+        return self._balance
 
     def Deposit(self, amount):
         self._balance += amount
@@ -37,17 +40,25 @@ class Account:
         return Account.NextAccountNumber
 
     def __str__(self):
-        print(self._accountNumber)
-        print(self._firstName)
-        print(self._lastName)
-        print(self._balance)
-    
-    def account_write(self):
-        write_lines=[]
-        write_lines.append(f'First Name: {self._firstName}')
-        write_lines.append(f'Last Name: {self._lastName}')
-        write_lines.append(f'Account Number: {self.getAccNo()')
-        write_lines.append(f'Balance: {self.getBalance()}')
-        return write_lines
-         
-         
+        out = []
+        out.append(f"First Name: {self._firstName}")
+        out.append(f"Last Name: {self._lastName}")
+        out.append(f"Account Number: {self.getAccNo()}")
+        out.append(f"Balance: {self.getBalance()}")
+        return "\n".join(out)
+
+    def account_read(self, lines):
+        self._accountNumber = lines.pop(0)
+        self._firstName = lines.pop(0)
+        self._lastName = lines.pop(0)
+        self._balance = lines.pop(0)
+
+    def account_write(self, filename):
+        f = open(filename,'a')
+        lines = f.readlines()
+        lines.append(f"{self._accountNumber}\n")
+        lines.append(f"{self._firstName}\n")
+        lines.append(f"{self._lastName}\n")
+        lines.append(f"{self._balance}\n")
+        f.writelines(lines)
+        f.close()
---
+++ bank_transaction_python/lib/bank.py
@@ -0,0 +1,47 @@
+from account import Account
+
+class Bank:
+    def __init__(self):
+        self.datafile_name = "Bank.data"
+        self.accounts = {}
+        try:
+            with open(datafile_name) as f:
+                bank_data_lines = f.readlines()
+                while bank_data_lines:
+                    acc = Account()
+                    acc.account_read(bank_data_lines)
+                    acc.setLastAccountNumber(acc.getAccNo())
+                    self.accounts[acc.getAccNo()] = acc
+        except IOError as error:
+            print("Faile to open file Bank.data")
+
+    def OpenAccount(self, fname, lname, balance):
+        account = Account(fname, lname, balance)
+        self.accounts[acc.getAccNo()] = account
+        account.write_lines(datafile_name)
+        return account
+
+    def BalanceEquiry(self, accountNumber):
+        # return the account class for displaying
+        return self.accounts[accountNumber]
+
+    def Deposit(self, accountNumber, amount):
+        accounts[accountNumber].Deposit(amount)
+        return self.accounts[accountNumber]
+
+    def WithDraw(self, accountNumber, amount):
+        account[accountNumber].WithDraw(amount)
+        return self.accounts[accountNumber]
+
+    def CloseAccount(self, accountNumber):
+        self.accounts.pop(accountNumber)
+
+    def ShowAllAccounts(self):
+        for key in self.account.keys():
+            print(self.accounts[key])
+
+    def __del__(self):
+        with open(self.datafile_name):
+            for account in self.accounts:
+                account.writelines(self.datafile_name)
+        print("Close with file written")
