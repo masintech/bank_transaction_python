a3e28f206ceb42b17ff5b0e222f97ba0e56ac486
weijeh <weijeh@avery-design.com.tw>
2021-04-13 18:52:18 +0800

    [import module]: solve module import problem Project: Note   :

--- bank_transaction_python/lib/bank.py
+++ bank_transaction_python/lib/bank.py
@@ -1,4 +1,9 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 from account import Account
+
 
 class Bank:
     def __init__(self):
---
+++ bank_transaction_python/main.py
@@ -0,0 +1,7 @@
+from lib.bank import Bank
+from lib.account import Account
+
+def main():
+
+if __name__ =='__main__':
+    main()
