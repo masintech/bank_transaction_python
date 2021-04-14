a55f9517b21f6bfe31d93941538d9f74184fad92
masintech <masintec@gmail.com>
2021-04-15 00:27:04 +0800

    diction to return a function address, menu list

--- bank_transaction_python/main.py
+++ bank_transaction_python/main.py
@@ -14,8 +14,11 @@ def print_instruct():
     print(f"{'6 Show All Accounts':<25}")
     print(f"{'7 Quit':<25}")
 
+
 def print_default_instruct():
     print("Please enter correct choice:")
+
+
 class BankClient:
     def __init__(self, bank):
         self.bank = bank
@@ -24,30 +27,55 @@ class BankClient:
         fname = input("Enter First Name: ")
         lname = input("Enter Last Name: ")
         balance = input("Enter initial Balance: ")
-        acc = self.bank.OpenAccount(fname, lname, balance)
+        acc = self.bank.OpenAccount(fname.strip(), lname.strip(), balance)
         print(acc)
 
+    def balance_enquiry(self):
+        accountNumber = input("Enter Account Number")
+        acc = self.bank.BalanceEquiry(accountNumber)
+        print(f"Your Account Details: {acc}")
+
+    def deposit(self):
+        pass
+
+    def withdraw(self):
+        pass
+
+    def close_account(self):
+        pass
+
+    def show_all_accounts(self):
+        for account in self.bank.accounts.values():
+            print(account)
+            print('\n')
+
+    def quit_system(self):
+        del self.bank
+        sys.exit()
 
     def get_choice(self, opt):
-        return {
-            "1": self.open_acc_instruct(),
-            # '2': 2 ,
-            # '3': 2 ,
-            # '4': 2 ,
-            # '5': 2 ,
-            # '6': 2 ,
-            # "7": sys.exit(),
+        funct= {
+            "1": self.open_acc_instruct,
+            "2": self.balance_enquiry,
+            "3": self.deposit,
+            "4": self.withdraw,
+            "5": self.close_account,
+            "6": self.show_all_accounts,
+            "7": self.quit_system,
         }.get(
-            opt, print_instruct()
+            opt, print_default_instruct
         )  # 9 is default if x not found
+        funct()
 
 
 def main():
-    b= Bank()
+    b = Bank()
     bank_client = BankClient(b)
-    while True:
-        print_default_instruct()
+    choice = 0
+    while choice!=7:
+        print_instruct()
         choice = input(f"{'Enter your choice: ':<25}")
+        print(f'Your choose {choice}')
         bank_client.get_choice(choice)
 
 
